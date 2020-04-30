f05f2f9d9d582bb2466e607200bbb7a624ac38e8
Nathanial Myers <nmyers322@gmail.com>
2020-04-30 02:29:42 -0700

    Disallow joining if max participants

---
+++ app/ParticipantsModal.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Button, Typography } from '@material-ui/core';
+import { withTheme } from '@material-ui/core/styles';
+import Person from '@material-ui/icons/Person';
+import Modal from '@material-ui/core/Modal';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+const ParticipantsModal = ({tableNumber, participants, maxParticipants, onClose, onJoin}) => 
+    <Modal
+        open={true}
+        onClose={() => {
+            onClose();
+        }}
+        style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            flexDirection: 'column',
+            outline: 'none'
+        }}>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            flexDirection: 'column',
+            backgroundColor: '#333333',
+            padding: '2vh',
+            border: '0',
+            borderRadius: '2vh 2vh',
+            outline: 'none'
+            }}>
+            <Typography color="textPrimary">
+                Table { tableNumber } - Current Participants
+            </Typography>
+            <List>
+                { participants.map(participant => 
+                    <ListItem key={participant}>
+                    <ListItemIcon><Person /></ListItemIcon>
+                        <Typography color="textPrimary">
+                            { participant }
+                        </Typography>
+                    </ListItem>) }
+            </List>
+            { participants.length >= maxParticipants &&
+                <Typography color="textPrimary">
+                    Table is full. Cannot join!
+                </Typography>
+            }
+            <Button 
+                variant="contained" 
+                color="default" 
+                size="large"
+                style={{marginTop: '3vh'}}
+                disabled={participants.length >= maxParticipants}
+                onClick={event => {
+                    onJoin();
+                    event.preventDefault && event.preventDefault();
+                }}>
+                Join Table
+            </Button>
+            <Button 
+                variant="outlined" 
+                color="default" 
+                size="small"
+                style={{marginTop: '3vh'}}
+                onClick={event => {
+                    onClose();
+                    event.preventDefault && event.preventDefault();
+                }}>
+                Close
+            </Button>
+        </div>
+    </Modal>;
+
+export default withTheme(ParticipantsModal);
--- app/TableIcon.js
+++ app/TableIcon.js
@@ -5,78 +5,26 @@ import { withTheme } from '@material-ui/core/styles';
 import Person from '@material-ui/icons/Person';
 import table_icon from './img/table_icon.png';
 import { iconStyles } from './styles/iconStyles';
-import Modal from '@material-ui/core/Modal';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import history from './util/history';
+import ParticipantsModal from './ParticipantsModal';
 
-const ParticipantsModal = withTheme(({tableNumber, participants, onClose, onJoin}) => 
-    <Modal
-        open={true}
-        onClose={() => {
-            onClose();
-        }}
-        style={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            flexDirection: 'column',
-            outline: 'none'
-        }}>
-        <div style={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            flexDirection: 'column',
-            backgroundColor: '#333333',
-            padding: '2vh',
-            border: '0',
-            borderRadius: '2vh 2vh',
-            outline: 'none'
-            }}>
-            <Typography color="textPrimary">
-                Table { tableNumber } - Current Participants
-            </Typography>
-            <List>
-                { participants.map(participant => 
-                    <ListItem key={participant}>
-                    <ListItemIcon><Person /></ListItemIcon>
-                        <Typography color="textPrimary">
-                            { participant }
-                        </Typography>
-                    </ListItem>) }
-            </List>
-            <Button 
-                variant="contained" 
-                color="default" 
-                size="large"
-                style={{marginTop: '3vh'}}
-                onClick={event => {
-                    onJoin();
-                    event.preventDefault && event.preventDefault();
-                }}>
-                Join Table
-            </Button>
-            <Button 
-                variant="outlined" 
-                color="default" 
-                size="small"
-                style={{marginTop: '3vh'}}
-                onClick={event => {
-                    onClose();
-                    event.preventDefault && event.preventDefault();
-                }}>
-                Close
-            </Button>
-        </div>
-    </Modal>);
+const maxParticipants = 5;
 
 const TableIcon = ({number, room, tableId}) => {
     const [participantsModal, setParticipantsModal] = useState(false);
+
+    const isMaxedOut = () => 
+        room && room.participants && room.participants.length >= maxParticipants;
+
     return (
         <div 
-            className="table-icon" 
+            className={isMaxedOut() ? "table-icon-disabled" : "table-icon"}
             style={iconStyles}
-            onClick={event => !participantsModal && history.push('/table/' + tableId)}>
+            onClick={event => {
+                    if (!participantsModal && !isMaxedOut()) {
+                        history.push('/table/' + tableId)
+                    }
+                }}>
             <Typography color="textPrimary">
                 Table { number }
             </Typography>
@@ -103,6 +51,7 @@ const TableIcon = ({number, room, tableId}) => {
                 <ParticipantsModal
                     tableNumber={number}
                     participants={room.participants}
+                    maxParticipants={maxParticipants}
                     onClose={() => setParticipantsModal(false)}
                     onJoin={() => history.push('/table/' + tableId)} />
             }
