ea0381d550553f463d43f590ac0666bf369aec21
Nathanial Myers <nmyers322@gmail.com>
2020-05-17 01:11:01 -0700

    remove live stream source

--- app/LiveStream.js
+++ app/LiveStream.js
@@ -2,15 +2,21 @@ import React from 'react';
 import VideoPlayer from './react-video-js-player/index.js';
 import { withRouter } from 'react-router-dom';
 
+const liveStreamSource = false;
+
 const LiveStream = ({history}) => {
-    return (history && history.location && history.location.pathname && (history.location.pathname.includes('table') || history.location.pathname.includes('lobby')))
+    return (!!liveStreamSource && 
+            history && 
+            history.location && 
+            history.location.pathname && 
+            (history.location.pathname.includes('table') || history.location.pathname.includes('lobby')))
         ? (
             <div className="livestream">
                 <VideoPlayer
                         controls={true}
                         autoplay={true}
                         playsInline={true}
-                        src={"http://206.189.78.238:80/hls/947f2adedf9830189393f034c1a63323.m3u8"}
+                        src={liveStreamSource}
                     />
             </div>
             )
